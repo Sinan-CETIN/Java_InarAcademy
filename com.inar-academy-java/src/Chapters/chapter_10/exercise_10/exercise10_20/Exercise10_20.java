a14c2635ec59726c0e39ea95623fc8a38a7665db
Sinan Cetin <sinanctn54@gmail.com>
2022-11-08 11:31:15 +0200

    exercise10_20

--- com.inar-academy-java/src/Chapters/chapter_10/exercise_10/exercise10_20/Exercise10_20.java
+++ com.inar-academy-java/src/Chapters/chapter_10/exercise_10/exercise10_20/Exercise10_20.java
@@ -4,29 +4,26 @@ import java.math.BigDecimal;
 
 public class Exercise10_20 {
     public static void main(String[] args) {
+        System.out.println(factor(5));
         displayNumberE();
     }
 
 
     public static void displayNumberE() {
         for (int i = 100; i <= 1000; i += 100) {
-            BigDecimal sum = new BigDecimal("1");
-
-            for (int j = 1; j <= i; j++) {
-                sum = sum.add(new BigDecimal(1 + "").divide(factor(new BigDecimal(j + "")), 25, BigDecimal.ROUND_UP));
+            BigDecimal sum = BigDecimal.ONE;
+            for (int j = 1; j < i; j++) {
+                BigDecimal increase = BigDecimal.ONE.divide(factor(i), 25, BigDecimal.ROUND_UP);
+                sum = sum.add(increase);
             }
-            System.out.println(sum);
+            System.out.println(i + " --> " + sum);
         }
     }
 
-    public static BigDecimal factor(BigDecimal number) {
+    public static BigDecimal factor(long number) {
         BigDecimal result = BigDecimal.ONE;
-        BigDecimal counter = BigDecimal.ONE;
-
-
-        while (counter.compareTo(number) != 0) {
-            result = result.multiply(counter);
-            counter.add(BigDecimal.ONE);
+        for (int i = 1; i <= number; i++) {
+            result = result.multiply(new BigDecimal(i + ""));
         }
         return result;
     }
