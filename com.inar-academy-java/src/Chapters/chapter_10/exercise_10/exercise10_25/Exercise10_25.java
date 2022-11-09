9a10b78337fd0efc0046115cbf217e1083a76c50
Sinan Cetin <sinanctn54@gmail.com>
2022-11-09 11:25:58 +0200

    Solved Exercise10_25

--- com.inar-academy-java/src/Chapters/chapter_10/exercise_10/exercise10_25/Exercise10_25.java
+++ com.inar-academy-java/src/Chapters/chapter_10/exercise_10/exercise10_25/Exercise10_25.java
@@ -2,86 +2,94 @@ package Chapters.chapter_10.exercise_10.exercise10_25;
 
 public class Exercise10_25 {
     public static void main(String[] args) {
-        String s = "Sinan/oasdgm/sadg,asdg/sdgkm  dasfa/sd gasdg/asd g/asfhbafsg ";
-        String[] split = split(s, "/");
-        System.out.println(s);
-        display(split);
-        String s2 = "Java dgkjandsg Java sad Java Java sdjg";
-        String[] split2 = split(s2, "Java");
-        System.out.println(s2);
-        display(split2);
-        System.out.println(checkEnd("Sinan", "an"));
+        String s = "ab?b?gf#e";
+        String regex = "[?#]";
+        String[] splittedString = split(s, regex);
+        display(splittedString);
+        System.out.println();
+        String[] splittedString1 = split("ab#12#453", "#");
+        display(splittedString1);
+        System.out.println();
+        String[] splittedString2 = split("?>ab#12#??>>453?", "[#?>]");
+        display(splittedString2);
+
+
+
     }
 
     public static String[] split(String s, String regex) {
 
-        int numberOfRegexInString = countRegex(s, regex);
-
-
-        if (numberOfRegexInString == 0) {
-            return new String[]{s};
+        if (regex.charAt(0) != '[' && regex.charAt(regex.length() - 1) != ']') {
+            regex = '[' + regex + ']';
         }
 
-        boolean hasRegexAtTheEnd = checkEnd(s, regex);
-        String[] result = new String[(hasRegexAtTheEnd ? numberOfRegexInString * 2 - 1 : numberOfRegexInString * 2)];
-        System.out.println(result.length);
+        int numberOfLength = findNumberOfLength(s, regex);
+        String[] result = new String[numberOfLength];
+        fillResult(result, s, regex);
+        return result;
+    }
 
-        String s1 = "";
+    public static int findNumberOfLength(String s, String regex) {
+        int firstIndex = -1;
+        int lastIndex = -1;
         int count = 0;
-        int countRegex = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (hasRegexIndex(s, regex, i)) {
-                countRegex++;
-                if (i != 0) {
-                    result[count++] = s1;
-                    s1 = "";
+            if (isRegex(s.charAt(i), regex)) {
+                if (i == 0) {
+                    lastIndex = i;
+                    count++;
+                    continue;
+                } else {
+                    firstIndex = lastIndex;
+                    lastIndex = i;
                 }
-
-                String s2 = s.substring(i, i + regex.length());
-                i += regex.length() - 1;
-                result[count++] = s2;
-
-                continue;
+                if (lastIndex - firstIndex == 1) {
+                    count++;
+                } else {
+                    count += 2;
+                }
             }
-            s1 += s.charAt(i);
-            if(countRegex == numberOfRegexInString && i == s.length() - 1) {
-                result[count - 1] = s1;
-            }
-
         }
-        return result;
+        return count;
     }
 
-    public static int countRegex(String s, String regex) {
-        int result = 0;
+    public static boolean isRegex(char ch, String regex) {
+        for (int i = 1; i < regex.length() - 1; i++) {
+            if (ch == regex.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void fillResult(String[] result, String s, String regex) {
+        int firstIndex = -1;
+        int lastIndex = -1;
+        int count = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (hasRegexIndex(s, regex, i)) {
-                result++;
+            if (isRegex(s.charAt(i), regex)) {
+                if (i == 0) {
+                    result[count++] = s.charAt(i) + "";
+                    lastIndex = i;
+                    continue;
+                } else {
+                    firstIndex = lastIndex;
+                    lastIndex = i;
+                }
+                if (lastIndex - firstIndex == 1) {
+                    result[count++] = s.charAt(i) + "";
+                } else {
+                    String s1 = s.substring(firstIndex + 1, lastIndex);
+                    result[count++] = s1;
+                    result[count++] = s.charAt(i) + "";
+                }
             }
         }
-        return result;
-    }
-
-    public static boolean hasRegexIndex(String s, String regex, int i) {
-        if (i + regex.length() > s.length()) return false;
-        return s.substring(i, i + regex.length()).equals(regex);
-    }
-
-    public static boolean checkEnd(String s, String regex) {
-        return s.substring(s.length() - regex.length()).equals(regex);
     }
 
     public static void display(String[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-
+            System.out.println(array[i]);
         }
-        System.out.println();
     }
 }
-
-/*
- sina|n| regex == ||
- | i = 4   regex.length() = 4 + 2 = 6   sina|n|.length() = 7
- */
-
