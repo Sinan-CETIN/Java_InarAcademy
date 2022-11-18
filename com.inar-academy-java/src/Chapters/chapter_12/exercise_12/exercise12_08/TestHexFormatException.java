8adc52fd11a8098250dac120805beed8e04d8616
Sinan Cetin <sinanctn54@gmail.com>
2022-11-18 11:48:48 +0200

    Solved Exercise12_27

--- com.inar-academy-java/src/Chapters/chapter_12/exercise_12/exercise12_08/TestHexFormatException.java
+++ com.inar-academy-java/src/Chapters/chapter_12/exercise_12/exercise12_08/TestHexFormatException.java
@@ -17,16 +17,13 @@ public class TestHexFormatException {
 
     public static void check(String s) {
         for (int i = 0; i < s.length(); i++) {
-            if(!isLegalHexValue(s.charAt(i))) {
+            if (!isLegalHexValue(s.charAt(i))) {
                 throw new HexFormatException(s);
             }
         }
     }
 
     public static boolean isLegalHexValue(char ch) {
-        if (('A' <= ch && ch <= 'F') || ('0' <= ch && ch <= '9')) {
-            return true;
-        }
-        return false;
+        return ('A' <= ch && ch <= 'F') || ('0' <= ch && ch <= '9');
     }
 }
--- com.inar-academy-java/src/Chapters/chapter_12/exercise_12/exercise12_27/Exercise12_27.java
+++ com.inar-academy-java/src/Chapters/chapter_12/exercise_12/exercise12_27/Exercise12_27.java
@@ -1,7 +1,44 @@
 package Chapters.chapter_12.exercise_12.exercise12_27;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Exercise12_27 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the directory's path --> ");
+        String path = input.nextLine();
+        File file = new File(path);
+        if (!file.isDirectory()) {
+            System.out.println(file.getName() + " is not a directory!!");
+            System.exit(1);
+        }
+        fixTheFileName(file);
+    }
+
+    public static void fixTheFileName(File file) {
+        File[] files = file.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            if (!files[i].isFile()) {
+                continue;
+            }
+            String name = files[i].getName();
+            name = name.substring(0, name.length() - 4);
+
+            String[] split = name.split("_");
+
+            if (split[1].length() == 1) {
+                split[1] = '0' + split[1];
+            }
+
+            if (!Character.isDigit(split[0].charAt(split[0].length() - 2))) {
+                split[0] = split[0].substring(0, split[0].length() - 1) + "0" + split[0].charAt(split[0].length() - 1);
+            }
+
+            String newName = split[0] + "_" + split[1];
+            File fileName = new File(files[i].getParent() + "\\"  + newName + ".txt");
+            files[i].renameTo(fileName);
+        }
     }
 }
